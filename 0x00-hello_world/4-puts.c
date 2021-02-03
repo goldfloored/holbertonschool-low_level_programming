9e3f17c52037e73592fbf9fa27b815af2dc02b0b
goldfloored <goldfloored@mail.com>
2021-02-03 13:07:52 +0000

    com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 /**
 * main - for a bit of
 * Return: 0this
 * str func with variable
 */
-int main()
+int main(void)
 {
-	char Str1[] = "Programming is like building a multilingual puzzle\n";
+	char Str1[];
 
+	strcpy(Str1, "Programming is like building a multilingual puzzle\n");
 	puts(Str1);
 	return (0);
 }
