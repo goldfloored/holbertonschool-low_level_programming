3d5f9a263f0504320520d058405ad520c5d74caf
goldfloored <goldfloored@mail.com>
2021-02-03 14:03:10 +0000

    com

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 /**
-* main - my ping it to ping them
-* Description: no one is available to help
-* Return: returned value is true
-*/
+ * main - my ping it to ping them
+ * Description: no one is available to help
+ * Return: returned value is true
+ */
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
