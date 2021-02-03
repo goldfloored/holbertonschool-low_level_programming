33f320518d2e0c1f8278ffed2ab24152e612a507
goldfloored <goldfloored@mail.com>
2021-02-03 13:32:18 +0000

    com

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
-int main() {
-    	int intType;
-    	float floatType;
-    	double doubleType;
+/**
+* main - newhelp
+* Description: noisnthtere
+* Return: returfkfn
+*/
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
 	char charType;
 	long long longlongType;
 
 	printf("Size of char: %zu bytes\n", sizeof(charType));
-    	printf("Size of int: %zu bytes\n", sizeof(intType));
-    	printf("Size of long int: %zu bytes\n", sizeof(doubleType));
-    	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(doubleType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	return(0);
+	return (0);
 }
