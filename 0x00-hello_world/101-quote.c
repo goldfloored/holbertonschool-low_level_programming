edc654dcb68e8b728f7fb635b5d41cffc7b137ae
goldfloored <goldfloored@mail.com>
2021-02-04 09:35:24 +0000

    com

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <sys/syscall.h>
 #include <unistd.h>
 /**
  * main - my ping it to ping them
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-
-	syscall(1, STDERR_FILENO, quote, sizeof(quote) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
