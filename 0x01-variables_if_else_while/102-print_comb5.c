a729005682cd4b85a1e4dd1980bb6236f2dfe8fb
goldfloored <goldfloored@mail.com>
2021-02-04 18:47:57 +0000

    com

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - function
+*Return: 0 as always
+*/
+int main(void)
+{
+	unsigned int x, e;
+
+	for (x = 0; x <= 99; ++x)
+{
+		for (e = x; e <= 99; ++e)
+{
+			if (x != e)
+{
+				if (x != 0 || e != 1)
+{
+					putchar(',');
+					putchar(' ');
+}
+				putchar(x / 10 + '0');
+				putchar(x % 10 + '0');
+				putchar(' ');
+				putchar(e / 10 + '0');
+				putchar(e % 10 + '0');
+}
+}
+}
+
+	putchar('\n');
+	return (0);
+}
