1545e3a90f5ce8018ddfb079652bb3f63df1981e
goldfloored <goldfloored@gmail.com>
2021-02-09 13:00:06 +0000

    com

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_alphabet - function
+ * print_alphabet_x10 - function * description: print 10 times
+ * Return: zero
+ **/
+
+void print_alphabet(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+int print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		print_alphabet();
+	}
+	return (0);
+}
