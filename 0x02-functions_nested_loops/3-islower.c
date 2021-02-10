39dd3e4decf2a77ceef42d80b2222afdafa941ed
goldfloored <goldfloored@gmail.com>
2021-02-10 18:52:28 +0000

    com

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
  *_islower - is lower than
+ * int c - is an ordinary
  *Return: 0
  **/
 int _islower(int c)
 {
-	if ((96 < c) && (c < 123))
+	if (96 < c && c < 123)
 		return (1);
 	else
 		return (0);
