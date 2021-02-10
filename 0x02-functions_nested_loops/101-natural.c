90ba4a463c4ae272f2b3e7a216e15d289adbe494
goldfloored <goldfloored@gmail.com>
2021-02-10 19:54:00 +0000

    com

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ /**
+ *main - main func
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int j;
+
+	j = 0;
+
+	i = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+		i++;
+	}
+	printf("%ld\n", j);
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,41 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_to_98 - all numbers from input to 98
- * @n: argument for indexing
- *
- * Return: void
+ *print_to_98 - main function
+ *@n: int
+ *Return: Void
  */
-
 void print_to_98(int n)
 {
-	if (n <= 98)
-		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
-	else
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-	printf("98\n");
+int i;
+
+if (n < 98)
+{
+for (i = n; i <= 98; ++i)
+{
+if (i != 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d\n", i);
+}
+}
+}
+else
+{
+for (i = n; i >= 98; --i)
+{
+if (i != 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d\n", i);
+}
+}
+}
+
 }
