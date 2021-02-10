7019fc75fe89854376324334aebe3b9aa3a7a561
goldfloored <goldfloored@gmail.com>
2021-02-10 19:14:47 +0000

    com

--- 0x02-functions_nested_loops/8-24-hours.c
+++ 0x02-functions_nested_loops/8-24-hours.c
@@ -5,18 +5,16 @@ void jack_bauer(void)
 
 	int m;
 
-	for (h = "00"; h <= "24"; h++)
+	for (h = 0; h <= 24; h++)
 	{
-		_putchar(h);
-		_putchar(":");
-		for (m = "00"; m <= "59"; m++)
+		for (m = 0; m <= 59; m++)
 		{
-			_putchar(h);
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
 			_putchar('\n');
-			if (m > 59)
-			{
-				
-			}
 		}
 	}
 
