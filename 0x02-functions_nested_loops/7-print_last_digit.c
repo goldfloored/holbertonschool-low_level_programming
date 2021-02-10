2ede59f2aa8642408773faacb3c6cfaec8e02fe8
goldfloored <goldfloored@gmail.com>
2021-02-10 19:05:35 +0000

    com

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,16 @@ int print_last_digit(int x)
 
 	if (n < 0)
 	{
-		n = -n;
+		n = (n % 10) * (-1);
+
+		_putchar(n + '0');
+		return (n);
 	}
-	_putchar(n + '0');
-	return (n);
+	else
+	{
+		n = n % 10;
+
+		_putchar(n + '0');
+		return (n);
+	}
 }
