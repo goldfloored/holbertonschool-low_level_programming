d4fae12840428f2bbb4a5a41bcc2afb7b469bfbe
goldfloored <goldfloored@gmail.com>
2021-02-10 09:26:46 +0000

    com

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,6 +5,16 @@
  * Return: zero
  **/
 
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
 void print_alphabet_x10(void)
 {
 	int x;
