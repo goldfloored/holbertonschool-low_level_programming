443c6b28cd5f569ec4798f751f49bafb89a4bfe7
goldfloored <goldfloored@gmail.com>
2021-02-11 11:59:28 +0000

    com

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,18 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 100; x++)
+	for (x = 1; x < 100; x++)
 	{
 		if (x % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (x % 5 == 0)
-			printf("Buzz");
-		else if (x % 15 == 0)
-			printf("FizzBuzz");
+			printf("Buzz ");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
 		else
-			printf("%d", x);
-		printf(" ");
+			printf("%d ", x);
 
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (1);
 }
