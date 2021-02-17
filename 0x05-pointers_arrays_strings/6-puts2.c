cdac3364748a1bffd51d7b22bc135beb3924208b
goldfloored <goldfloored@gmail.com>
2021-02-17 15:10:43 +0000

    com

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,17 @@
  */
 void puts2(char *str)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{}
-
-	for (j = 0; j < i; j += 2)
+	i = 0;
+	while (*str != '\0')
 	{
-		_putchar(str[j]);
-}
-	_putchar('\n');
+		if (i % 2 == 0)
+		{
+		_putchar(*str);
+		}
+	str++;
+	i++;
+	}
+_putchar('\n');
 }
