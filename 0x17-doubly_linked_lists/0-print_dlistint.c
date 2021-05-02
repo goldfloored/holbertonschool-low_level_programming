ff6f49097b65d3b77675b336a972e46a2451f307
goldfloored <goldfloored@gmail.com>
2021-05-02 03:06:13 +0000

    com

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,12 +5,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-size_t	len = 0;
+	int len = 0;
 
-	while (h != NULL) {
+	while (h) {
 		printf("%d\n", h->n);
+		len++;
 		h = h->next;
-		len= len + 1;
 	}
 	return(len);
 }
